d39456b42cb098ef4702a26d7e453e64cb2cdfc4
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-11 13:17:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,5 +31,14 @@ bot.cmd('nickff', async (ctx) => {
     return await bot.sendMessage(ctx, '<i>Nick Epep Random</i> :<code>'+bru+'</code>', { parse_mode: 'html' });
     })
 
+bot.cmd('alive', async (ctx) => {
+    // message in only
+        let yah = performance.now();
+        let kek = performance.now();      
+        let k = '<code>' + ((kek - yah) / 1000).toLocaleString('id-ID', { maximumFractionDigits: 3 }) + "</code>"
+        let das = bot.peerGetId(ctx);
+        return await bot.sendMessage(ctx, `Nekozu Userbot Is Alive!\nOwner ID: ${das}\nUserbot Version: <code>1.2.0</code>\nUptime: ${k}.`, { parse_mode: 'html' });
+});
+
 
 bot.start();
