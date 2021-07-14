22a75ee8871e7ff5070cb1d39b05c2bf2319e411
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-14 07:31:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -13,27 +13,8 @@ const bot = new duaGram({
 
 let admin = Number(process.env.admin_id)
 
-// credit https://t.me/ubotindonesia/782
-function sudoacces(arraysudo, id) {
-        if (arraysudo.indexOf(id) > -1) {
-            return true;
-        } else {
-            return false;
-        }        
-    }
-
-bot.middleware((ctx, next) => {
-
-    var array_admin = [admin]
-
-if (! sudoacces(array_admin, bot.getPeerId(ctx))) return false
-    next();
-});
-
-
-
 bot.cmd('ping', async (ctx) => {
-    sudoacces
+    if (admin)
     let t0 = performance.now();
     let t1 = performance.now();
     let diff = ((t1 - t0) / 1000).toLocaleString('id-ID', { maximumFractionDigits: 3 });
@@ -41,21 +22,21 @@ bot.cmd('ping', async (ctx) => {
 });
 
 bot.cmd('id', async (ctx) => {
-      sudoacces
+      if (admin)
       let chat_id = bot.getPeerId(ctx);
       // let idd = bot.peerGetId;
        return await bot.sendMessage(ctx, 'ID: '+chat_id)
 })
 
-bot.cmd('nickff', async (ctx) => { 
-    sudoacces                                                                                                                                                                                        
+bot.cmd('nickff', async (ctx) => {  
+    if (admin)                                                                                                                                                                                       
     const nani = await axios.get('https://api.zeks.xyz/api/nickepep?apikey=apivinz')
     const bru = nani.data.result                                                                                                                                                                                                                                                                                                                                                     
     return await bot.sendMessage(ctx, '<i>Nick Epep Random</i> :<code>'+bru+'</code>', { parse_mode: 'html' });
     })
 
 bot.cmd('alive', async (ctx) => {
-        sudoacces
+        if (admin)
     // message in only
         let yah = performance.now();
         let kek = performance.now();      
@@ -64,22 +45,21 @@ bot.cmd('alive', async (ctx) => {
         return await bot.sendMessage(ctx, `Nekozu Userbot Is Alive!\nOwner ID: <code>${das}</code>\nUserbot Version: <code>1.2.0</code>\nUptime: ${k}\n\n<a href="https://t.me/nekozu">Update Channel</a>`, { parse_mode: 'html' });
 });
 
-bot.cmd('berita', async (ctx) => {   
-    sudoacces                                                                                                                                                                                      
+bot.cmd('berita', async (ctx) => {  
+    if (admin)                                                                                                                                                                                       
     const news = await axios.get('http://fadhil-a.herokuapp.com/api/berita.php?')
     const api = news.data.data                                                                                                                                                                                                                                                                                                                                                     
     return await bot.sendMessage(ctx, 'Judul Berita: '+api.judul+'Link Berita: '+api.link);
     })
 
 bot.cmd('help', async (ctx) => {
-        sudoacces
+        if (admin)
         let pesan = 'Command Available\n<code>alive, ping, berita, nickff, id</code>'
         return await bot.sendMessage(ctx, pesan, { parse_mode: 'html' });
 })
 
 // lagi error yg mau bantuin silahkan
 bot.cmd('pin', async (ctx) => {
-        sudoacces
         let result = bot.reply_to_message_id
         let message_id;
         if (result.updates) {
@@ -94,8 +74,8 @@ bot.cmd('unpinall', async (ctx) => {
         return await bot.unpinAllMessages(ctx)
 })
 
-bot.cmd('gempa', async (ctx) => {
-    sudoacces                                                                                                                                                                                         
+bot.cmd('gempa', async (ctx) => {     
+    if (admin)                                                                                                                                                                                    
     const gem = await axios.get('https://mhankbarbar.herokuapp.com/api/infogempa?')
     const pa = gem.data                                                                                                                                                                                                                                                                                                                                                    
     return await bot.sendMessage(ctx, 'Kedalaman: '+pa.kedalaman+'\Kordinat: '+pa.koordinat+'\nLokasi: '+pa.lokasi+'\nMagnitude: '+pa.magnitude+'\nPotensi: '+pa.potensi+'\nWaktu: '+pa.waktu);
